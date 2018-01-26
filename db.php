26d15d029c5f08677f1f50ecd20a3f66d9aba914
Neekos <and1997310366@mail.ru>
2018-01-26 19:02:45 +0700

    how are you?

--- db.php
+++ db.php
@@ -1,1 +1,15 @@
-db.php
+<?php 
+	/**
+	* Наш первый класс
+	*/
+	class ClassName extends AnotherClass
+	{
+		public $k = "Здорово!";
+		function super()
+		{
+			$this->k = "Просто супер!":
+		}
+	}
+	$norm = new ClassName();
+	echo $norm->super();
+ ?>
